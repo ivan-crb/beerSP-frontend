57a31e948c1cd7a3fd9598aa3c52876162d38297
ivan-crb <caraballomesaivan@gmail.com>
2025-12-09 23:41:07 +0100

    cervezas favoritas

--- www/js/main-page.js
+++ www/js/main-page.js
@@ -3,15 +3,62 @@ if (!window.cordova) document.addEventListener('DOMContentLoaded', onDeviceReady
 
 let baseURL = "https://beersp-latest.onrender.com";
 
-function onDeviceReady() {
+async function onDeviceReady() {
     console.log("READY")
 
     let user = JSON.parse(sessionStorage.getItem('user'));
     
     let usernameElement = document.getElementById("profile-username")
     let imageElement = document.getElementById("profile-image")
+    let cervezasFavoritas = document.getElementById("cervezas-favoritas")
 
     usernameElement.innerText = user.nombreUsuario
     if (user.foto != null) imageElement.src = `${user.foto}`
     else imageElement.classList.add("hidden")
+
+    let res = await fetch(`${baseURL}/cervezas/usuarios/${user.id}`, {
+        method: "GET"
+    })
+
+    if (res.status != 200) {
+        if (window.cordova) navigator.notification.alert("Hubo un error al cargar las cervezas favoritas", nothing, "Error", "Ok")
+        else alert("Hubo un error al cargar las cervezas favoritas")
+        return
+    }
+
+    let body = await res.json()
+    
+    for (let i = 0; i < body.length; i++) {
+        let itemContainer = document.createElement("div")
+        itemContainer.classList.add("item-container")
+        itemContainer.classList.add("beer-container")
+
+        let beerText = document.createElement("div")
+        beerText.classList.add("beer-text")
+
+        let beer = document.createElement("div")
+        beer.classList.add("beer-name")
+        beer.classList.add("highlight")
+        beer.innerText = body[i].nombre
+
+        let mean = document.createElement("div")
+        mean.classList.add("beer-mean")
+        if (body[i].promedio != null) mean.innerText = `Promedio: ${Math.round(body[i].promedio*100)/100}`
+        else mean.classList.add("hidden")
+
+        let img = document.createElement("img")
+        img.classList.add("beer-image")
+        if (body[i].foto != null) img.src = `${body[i].foto}`
+        else img.classList.add("hidden")
+
+        beerText.appendChild(beer)
+        beerText.appendChild(mean)
+
+        itemContainer.appendChild(beerText)
+        itemContainer.appendChild(img)
+        
+        cervezasFavoritas.appendChild(itemContainer)
+    }
+
+
 }
